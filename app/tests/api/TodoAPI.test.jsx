5fcce15e8849af0318e24f57bfa06f800db75b45
TychoCRD <rtcotton@gmail.com>
2016-08-27 14:39:02 +0800

    Wire up search and filter functionality and test

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -14,7 +14,7 @@ describe('TodoAPI', ()=>{
   describe('setTodos', ()=>{
     it('should set valid todos array', ()=>{
       var todos = [{
-        it: 1,
+        id: 1,
         text: 'test it',
         completed: false
       }];
@@ -42,7 +42,7 @@ describe('TodoAPI', ()=>{
 
     it('should return todo if valid array in localStorage', ()=>{
       var todos = [{
-        it: 1,
+        id: 1,
         text: 'test it',
         completed: false
       }];
@@ -56,5 +56,52 @@ describe('TodoAPI', ()=>{
 
   });
 
-  describe('getTodos', ()=>{});
+  describe('filteredTodos', ()=>{
+    var todos = [{
+      id: 1,
+      text: 'test it',
+      completed: true
+    },
+    {
+      id: 2,
+      text: 'walk the dog',
+      completed: false
+    },
+    {
+      id: 3,
+      text: 'run test',
+      completed: true
+    }];
+
+    it('should return all items if showCompleted is true', ()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should only show incomplete items if showCompleted is false', ()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', ()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by searchText', ()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'run');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should return all todos if searchText is empty', ()=>{
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+  });
 });
