e5a08d3340789efa3c1e37463986c152e0e0afef
TychoCRD <rtcotton@gmail.com>
2016-09-08 14:02:04 +0800

    Update todos on firebase

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -4,14 +4,34 @@ var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
+import * as actions from 'actions';
+import {Todo} from 'Todo';
 // var Todo = require('Todo');
-
-var {Todo} = require('Todo');
+// var {Todo} = require('Todo');
 
 describe('Todo', ()=>{
   it('should exist', ()=>{
     expect(Todo).toExist();
   });
+
+  it('should dispatch TOGGLE_TODO action on click', ()=>{
+    var todoData = {
+      id: 199,
+      text: 'Write todo test',
+      completed: false
+    };
+    var action = actions.startToggleTodo(todoData.id, !todoData.completed);
+    var spy = expect.createSpy();
+    var todo = TestUtils.renderIntoDocument(<Todo {...todoData} dispatch={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(todo));
+
+    TestUtils.Simulate.click($el[0]);
+
+    expect(spy).toHaveBeenCalledWith(action);
+  });
+
+});
+
 
   // it('should call onToggle prop with id on click', ()=>{
   //   var todoData = {
@@ -28,23 +48,3 @@ describe('Todo', ()=>{
   //   expect(spy).toHaveBeenCalledWith(199);
   //
   // });
-
-  it('should dispatch TOGGLE_TODO action on click', ()=>{
-    var todoData = {
-      id: 199,
-      text: 'Write todo test',
-      completed: false
-    };
-    var spy = expect.createSpy();
-    var todo = TestUtils.renderIntoDocument(<Todo {...todoData} dispatch={spy}/>);
-    var $el = $(ReactDOM.findDOMNode(todo));
-
-    TestUtils.Simulate.click($el[0]);
-
-    expect(spy).toHaveBeenCalledWith({
-      type: 'TOGGLE_TODO',
-      id: todoData.id
-    });
-  });
-
-});
